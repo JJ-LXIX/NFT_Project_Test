5b0b1ce2799bce96ceccfc7a6fffeb75421a5711
Jagrath <jagrathshettylxix@gmail.com>
2022-04-07 14:42:24 +0530

    Content added to Homepage

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -20,14 +20,25 @@ const NFTDropPage = () => {
       </div>
 
       
-    
+  
 
      {/* Right Side */}
 
-     <div>
+     <div className='flex flex-1 flex-col p-12 lg:col-span-6'>
        {/* Header */}
+       <header className="flex items-center justify-between">
+         <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-80">The <span className='font-extrabold text-xl underline decoration-pink-600/50'>NOTMYART</span> NFT Market Place</h1>
+         <button className='rounded-full text-white px-4 bg-rose-400 py-2 text-xs font-bold lg:px-5 lg:py-3 lg:text-base'>Sign In</button>
+       </header>
+
+       <hr  className='my-2 border'/>
 
        {/* Content */}
+       <div className='mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center lg:space-y-0'>
+         <img className='w-80 object-cover pb-10 lg:h-40' src="https://links.papareact.com/bdy" alt="" />
+         <h1 className='text-3xl font-bold lg:text-5xl lg:font-extrabold'>The NOTMYART Ape Auto Rickshaw Club | NFT Drop</h1> 
+         <p className='pt-2 text-xl text-green-500'> 13/50 NFT's claimed</p> 
+       </div>
 
        {/* Mint Button */}
      </div>
