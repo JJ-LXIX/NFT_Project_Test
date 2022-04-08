f5d3d727865d3a6c7b72fb039f37d5734b44e7db
Jagrath <jagrathshettylxix@gmail.com>
2022-04-08 12:37:11 +0530

    ServerSideRendering implementation added (half done)

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,15 @@ import type { NextPage ,GetServerSideProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import {sanityClient , urlFor } from '../sanity';
+import { Collection } from '../typings';
 
-const Home: NextPage = () => {
+
+interface Props{
+  collections : Collection[]
+}
+
+const Home = ({collections} : Props) => {
   return (
     <div className="">
       <Head>
@@ -23,3 +30,38 @@ const Home: NextPage = () => {
 
 export default Home
 
+export const getServerSideProps: GetServerSideProps = async () => {
+  const query = `*[_type == "collection"]{
+    _id,
+    title,
+    address,
+    description,
+    nftCollectionName,
+    mainImage{
+        asset
+    },
+    previewImage{
+        asset
+    },
+    slug{
+        current
+    },
+    creator-> {
+        _id,
+        name,
+        address,
+        slug {
+            current
+        },
+    },
+} `
+
+const collections = await sanityClient.fetch(query)
+console.log(collections);
+
+return {
+  props:{
+    collections
+  }
+}
+}
