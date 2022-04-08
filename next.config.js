7ecbfcf1f11e00024e235af091160fcf9b8245f0
Jagrath <jagrathshettylxix@gmail.com>
2022-04-08 12:26:05 +0530

    changed few sanity config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.PUBLIC_SANITY_PROJECT_ID,
+    NEXT_PUBLIC_SANITY_DATASET: process.env.PUBLIC_SANITY_DATASET,
+    SANITY_API_TOKEN: process.env.SANITY_API_TOKEN,
+  },
 }
