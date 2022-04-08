626c851d041c7da2df81ca536d4d0ae9356c9bad
Jagrath <jagrathshettylxix@gmail.com>
2022-04-08 11:43:37 +0530

    sanity cms and thirdweb

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from 'next'
+import type { NextPage ,GetServerSideProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -17,12 +17,9 @@ const Home: NextPage = () => {
           <button className="p-2 rounded-full border border-rose-500 w-30 lg:w-40 text-rose-500 text-xl font-extrabold tracking-wider">Enter</button>
         </Link>
       </div>
-      
-      
-
-      
     </div>
   )
 }
 
 export default Home
+
